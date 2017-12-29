280740087ee205677f76ff9287de50b2cd1f76a7
radim <radvecerek@gmail.com>
2017-12-29 20:52:04 +0100

    java annot config

---
+++ src/base/SportConfig.java
@@ -0,0 +1,10 @@
+package base;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("base")
+public class SportConfig {
+
+}
